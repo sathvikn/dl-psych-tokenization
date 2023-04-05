1bfab11c181cdca35dfabe55679251001bcd99af
Sathvik Nair <sathviknair@berkeley.edu>
2023-04-05 15:31:36 -0400

    append word boundary characters to BPE tokenization

--- process_coca_lib.py
+++ process_coca_lib.py
@@ -1,8 +1,10 @@
+import argparse
 import pickle
+from transformers import AutoTokenizer
+
 import importlib
 import LiB
 importlib.reload(LiB)
-import argparse
 
 def load_pickle(filename):
     with open(filename, "rb") as f:
@@ -32,18 +34,32 @@ def train_model(processed_corpus):
     m.show_result(coca_corpus[:10])
     return m
 
-def write_outputs(processed_corpus, model, filename):
+def write_outputs(processed_corpus, filename, model = None):
     with open(filename, "w") as f:
         for sentence in processed_corpus:
-            f.write(" ".join(model.show_reading(sentence, return_chunks = True)) + "\n")
+            processed_sentence = sentence
+            if model:
+                processed_sentence = model.show_reading(sentence, return_chunks = True)
+                f.write("|".join(processed_sentence) + "\n") # | is the delimiter for token boundaries
+            else:
+                f.write(processed_sentence + "\n")
 
 
-if "__name__" == __main__:
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type = str, required = True)
-    parser.add_argumnt("--output", type = str, required = True)
+    parser.add_argument("--output", type = str, required = True)
+    parser.add_argument("--model", type = str, required = False)
     args = parser.parse_args()
 
     corpus = process_corpus(load_pickle(args.input))
-    model = train_model(corpus)
-    write_outputs(corpus, model, args.output)
+    # this should prob be cleaned up: make a constant dictionary or something?
+    if args.model == "LiB":
+        lib_model = train_model(corpus)
+        write_outputs(corpus, args.output, model = lib_model)
+    elif args.model == "BPE":
+        tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        bpe_corpus = ["Ġ" + " ".join(tokenizer.tokenize(sentence)) for sentence in corpus] # figure out how to get the word boundary token from the library
+        write_outputs(bpe_corpus, args.output)
+    else:
+        write_outputs(corpus, args.output)
