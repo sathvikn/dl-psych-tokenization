de0a98c16ba60645dab43ecfc828960afc6dbccc
Sathvik Nair <sathviknair@berkeley.edu>
2023-05-07 23:07:59 -0400

    compute sentence-level log likelihood

--- generate_surprisal_estimates.py
+++ generate_surprisal_estimates.py
@@ -1,15 +1,18 @@
 import argparse
 import os
 from typing import Dict, List
+
 import kenlm
 import pandas as pd
-from transformers import AutoTokenizer
+import numpy as np
 
+from transformers import AutoTokenizer
+from tqdm import tqdm
 
 def process_dundee_corpus(path: str, models: List[Dict]):
     averaged_times = [f for f in os.listdir(path) if f.endswith("_avg.txt")] # file name
     surprisal_rt = []
-    for textfile in averaged_times:
+    for textfile in tqdm(averaged_times):
         current_sentence = []
         total_rt = 0
         file_lines = open(os.path.join(path, textfile), 'r').readlines()
@@ -17,12 +20,11 @@ def process_dundee_corpus(path: str, models: List[Dict]):
             current_word, word_rt = line.split()
             current_sentence.append(current_word)
             total_rt += float(word_rt)
-            if "." in line: # other conditions for end sentence, also condition to exclude things
-                # TODO: how do we stop this thing from spitting out UNIGRAM surprisal??
-                surprisal_rt.append(compute_sentence_surprisal(" ".join(current_sentence), total_rt, models))
+            if "." in current_word or "?" in current_word or "!" in current_word: # other conditions for end sentence, also condition to exclude things
+                # TODO: up to index -1 is to exclude punctuation
+                surprisal_rt.append(compute_sentence_surprisal(" ".join(current_sentence)[:-1], total_rt, models))
                 current_sentence = []
                 total_rt = 0 
-        break
     return pd.DataFrame(surprisal_rt)
     
 def load_models(model_dir: str):
@@ -40,12 +42,13 @@ def load_models(model_dir: str):
 def compute_sentence_surprisal(sentence: List[str], total_rt: float, models: List[Dict]):
     # for each model
     # tokenize sentence & compute surprisal
-    sentence_rt_surprisals = {'sentence': sentence, 'total_rt': total_rt, 'word_count': len(sentence)}
+    sentence_rt_surprisals = {'sentence': sentence, 'total_rt': total_rt, 'word_count': len(sentence.split(" "))}
     for model_dict in models:
         processed_sentence = " ".join(sentence)
         model_name = model_dict['name']
         if 'tokenizer' in model_dict.keys(): # HACK, just works w/GPT2 tokenization now
             processed_sentence = "Ġ" + " ".join(model_dict['tokenizer'].tokenize(processed_sentence))
+        # score is log prob (log 10)
         sentence_rt_surprisals[f'{model_name}_surprisal'] = model_dict['lm'].score(processed_sentence, bos = True, eos = True)
     return sentence_rt_surprisals
 
