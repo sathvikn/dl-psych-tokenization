5bb6989d8f59182ffc26d53fc6ec2a31a27fc0d3
Sathvik Nair <sathviknair@berkeley.edu>
2023-05-09 22:36:14 -0400

    save raw data from dundee corpus into CSV

---
+++ process_dundee_corpus.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import re
+
+CORPUS_DIR="data/human_rts/dundee"
+
+rt_data = []
+averaged_times = [f for f in os.listdir(CORPUS_DIR) if f.endswith("_avg.txt")] # file name
+
+for filename in averaged_times:
+    transcript_id = re.findall("[0-9]+", filename).pop() # finds numbers in transcript
+    file_lines = open(os.path.join(CORPUS_DIR, filename), 'r').readlines()
+    rt_data += [{"token": line.split()[0], "RT": float(line.split()[1]), 
+                "transcript_id": transcript_id} for line in file_lines]
+
+rt_df = pd.DataFrame(rt_data)
+rt_df.index.name = "token_uid"
+rt_df.to_csv("dundee_rts.csv")
