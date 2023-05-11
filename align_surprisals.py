1a409c38b1c1e56b3d79c6a7eefdc549c14cac6a
Sathvik Nair <sathviknair@berkeley.edu>
2023-05-11 17:00:48 -0400

    fix:make all token comparisons lowercase and handle OOV items w/tokenized text

--- align_surprisals.py
+++ align_surprisals.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 def combine_data(rt_data: pd.DataFrame, surprisals: pd.DataFrame, word_boundary = ''):
     rt_surprisals = []
-    # maybe have indices/pointers to where we are in each dataframe instead of iterating over rows
     rt_iterator, rt_columns = rt_data.itertuples(name = None), rt_data.columns.values.tolist()
     surprisal_iterator, surprisal_columns = surprisals.itertuples(name = None), surprisals.columns.values.tolist()
     token_index = 0
@@ -11,16 +10,16 @@ def combine_data(rt_data: pd.DataFrame, surprisals: pd.DataFrame, word_boundary 
         token_index += 1
         current_word, current_token = current_word[1:], current_token[1:] # getting rid of index column
         buffer = {column:value for value, column in zip(current_token[1:], surprisal_columns[1:])}
-        if word_boundary in buffer['token']:
-            buffer['token'] = buffer['token'][1:]
-        mismatch = buffer['token'] != current_word[rt_columns.index('token')].lower()
-        print(buffer['token'], current_word[rt_columns.index('token')])
-        # print(buffer['token'], current_word[rt_columns.index('token')])
+        if word_boundary and word_boundary in buffer['token']:
+            buffer['token'] = buffer['token'][1:] # remove the word boundary character
+        mismatch = buffer['token'].lower() != current_word[rt_columns.index('token')].lower()
         while mismatch:
             current_token = next(surprisal_iterator)[1:]
             token_index += 1
             buffer['token'] += current_token[surprisal_columns.index('token')]
             buffer['token_score'] += current_token[surprisal_columns.index('token_score')]
+            if not buffer['oov'] and current_token[surprisal_columns.index('oov')]:
+                buffer['oov'] = True
             if buffer['token'] == current_word[rt_columns.index('token')].lower():
                 mismatch = False
         buffer['rt'] = current_word[rt_columns.index('RT')]
@@ -28,4 +27,4 @@ def combine_data(rt_data: pd.DataFrame, surprisals: pd.DataFrame, word_boundary 
         buffer = {"surprisal" if k == 'token_score' else k:v for k,v in buffer.items()}
         rt_surprisals.append(buffer)
         buffer = {}
-    return rt_surprisals
+    return pd.DataFrame(rt_surprisals)
