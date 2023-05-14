9fadca6c5a9642e11edd3ec2c79c80dd19d52388
Sathvik Nair <sathviknair@berkeley.edu>
2023-05-14 17:07:17 -0400

    add predictors

--- align_surprisals.py
+++ align_surprisals.py
@@ -30,4 +30,22 @@ def combine_data(rt_data: pd.DataFrame, surprisals: pd.DataFrame, word_boundary 
     return pd.DataFrame(rt_surprisals)
 
 def word_length(rt_data: pd.DataFrame, token_col: str):
-    return rt_data.apply(len, 'token_col', axis = 1)
+    return rt_data.apply(lambda row: len(row[token_col]), axis = 1)
+
+def join_log_freq(filepath: str, rt_data: pd.DataFrame):
+    freq_table = pd.read_table(filepath, delim_whitespace=True, names=('prob', 'word', 'backoff_weight'))
+    rt_data = rt_data.merge(freq_table[['prob', 'word']], how = 'left', left_on = 'token', right_on = 'word')
+    rt_data.rename(columns={'prob': 'log_freq'}, inplace = True)
+    return rt_data
+
+def prev_token_predictors(rt_data: pd.DataFrame):
+    rt_data['prev_freq'] = rt_data['log_freq'].shift(1)
+    rt_data['prev_len'] = rt_data['word_length'].shift(1)
+    rt_data['prev_surprisal'] = rt_data['token_score'].shift(1)
+    return rt_data
+
+def generate_predictors(rt_data: pd.DataFrame):
+    rt_data['word_length'] = word_length(rt_data, 'token')
+    rt_data = join_log_freq('word_freqs.txt', rt_data)
+    rt_data = prev_token_predictors(rt_data)
+    return rt_data.dropna()
