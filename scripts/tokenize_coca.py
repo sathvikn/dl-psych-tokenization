087a603c8e8736053f722c44d60fbd459c96cfd4
Sathvik Nair <sathviknair@berkeley.edu>
2023-05-19 13:43:21 -0400

    add module to use morphological transducer & tokenize COCA

--- scripts/tokenize_coca.py
+++ scripts/tokenize_coca.py
@@ -1,10 +1,12 @@
 import argparse
 import pickle
+import sys
+from typing import List
+
 from transformers import AutoTokenizer
 
-import importlib
-import LiB
-importlib.reload(LiB)
+sys.path.append("..")
+from src import morph_segmenter
 
 def load_pickle(filename):
     with open(filename, "rb") as f:
@@ -18,38 +20,17 @@ def process_corpus(coca):
             coca_sents.append(("".join(sentence)).strip())
     return coca_sents
 
-def train_model(processed_corpus):
-    model = LiB.model
-    coca_corpus = model.create_corpus(coca_sents)
-    m = model(coca_corpus, lexicon_in=0.25, lexicon_out=0.0001, update_rate=0.2, life=3)
-    m.logs['note'] = 0
-    for epoch_id in range(0,501):
-        m.run(epoch_id, article_length=200, test_interval=100);
-        if epoch_id > 1 and epoch_id % 100 == 0:
-            break
-
-    print(f"Time cost for training: {sum(m.logs['time_cost'])//60} min {sum(m.logs['time_cost'])%60:.1f}s\n")
-
-    print('Raw/LiB segmentation:')
-    m.show_result(coca_corpus[:10])
-    return m
-
-def write_outputs(processed_corpus, filename, model = None):
+def write_outputs(processed_corpus: List[str], filename: str, model = None):
     with open(filename, "w") as f:
         for sentence in processed_corpus:
-            processed_sentence = sentence
-            if model:
-                processed_sentence = model.show_reading(sentence, return_chunks = True)
-                f.write("|".join(processed_sentence) + "\n") # | is the delimiter for token boundaries
-            else:
-                f.write(processed_sentence + "\n")
-
+            f.write(sentence + "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type = str, required = True)
     parser.add_argument("--output", type = str, required = True)
     parser.add_argument("--model", type = str, required = False)
+    parser.add_argument("--model_path", type = str, required = False)
     args = parser.parse_args()
 
     corpus = process_corpus(load_pickle(args.input))
@@ -61,5 +42,8 @@ if __name__ == "__main__":
         tokenizer = AutoTokenizer.from_pretrained("gpt2")
         bpe_corpus = ["Ġ" + " ".join(tokenizer.tokenize(sentence)) for sentence in corpus] # figure out how to get the word boundary token from the library
         write_outputs(bpe_corpus, args.output)
+    elif args.model == "transducer":
+        transducer_results = morph_segmenter.tokenize_corpus(args.model_path, corpus)
+        write_outputs(transducer_results, args.output)
     else:
         write_outputs(corpus, args.output)
---
+++ src/morph_segmenter.py
@@ -0,0 +1,90 @@
+import argparse
+import json
+import os
+from typing import List
+from tqdm import tqdm
+
+import torch
+
+from trans import transducer
+from trans import vocabulary
+from trans import sed
+from trans import optimal_expert_substitutions
+from trans import utils
+from trans.train import decode
+
+def tokenize_sentence(model: transducer, vocab: vocabulary, sentence: str):
+    dataloader = prep_data(sentence, vocab, batch_size = 5)
+    results = predict(model, dataloader)
+    tokenized = " ".join([insert_wb_char(token.split("\t")[1]) for token in results])
+    return tokenized
+    
+def tokenize_corpus(model_dir: str, sentence_list: List[str]):
+    sentence_list = sentence_list
+    model, vocab = load_model_and_vocab(model_dir)
+    dataloader = prep_data(sentence_list, vocab, batch_size = 1000)
+    results = predict(model, dataloader)
+    return output_as_sentences(results, sentence_list)
+
+def load_model_and_vocab(model_dir:str):
+    model_path = os.path.join(model_dir, "best.model")
+    config_path = os.path.join(model_dir, "config.json")
+    vocab_path = os.path.join(model_dir, "vocabulary.pkl")
+    stochastic_edit_distance_params = os.path.join(model_dir, "sed.pkl")
+    vocab = vocabulary.Vocabularies.from_pickle(vocab_path)
+
+    stochastic_edit_distance_aligner = sed.StochasticEditDistance.from_pickle(stochastic_edit_distance_params)
+    expert = optimal_expert_substitutions.OptimalSubstitutionExpert(stochastic_edit_distance_aligner)
+
+    with open(config_path, "r") as c:
+            model_config: dict = json.load(c)
+    args_dict = {}
+    for key, value in model_config.items():
+        args_dict[key.replace("-", "_")] = value
+    args_dict['device'] = 'cpu'
+    print("loading model")
+    model = transducer.Transducer(vocab, expert, argparse.Namespace(**args_dict))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    return model, vocab
+
+def prep_data(sentence_list: List[str], vocab: vocabulary.Vocabularies, batch_size: int):
+    # the transducer accepts word-by-word (or token-by-token)
+    test_data = utils.Dataset()
+    print("reading sentences into Dataloader")
+    for sentence in tqdm(sentence_list):
+        sentence_tokens = sentence.strip().split()
+        for token in sentence_tokens:
+            encoded_input = torch.tensor(vocab.encode_unseen_input(token))
+            sample = utils.Sample(token, None, encoded_input) # the target token goes where None should be, this isn't necessary since we're just running inference
+            test_data.add_samples(sample)
+    return test_data.get_data_loader(batch_size=batch_size)   
+
+def predict(model: transducer, data_loader: torch.utils.data.DataLoader):
+    results = []
+    print("making predictions")
+    with torch.no_grad():
+        predictions = decode(model, data_loader).predictions
+        results += predictions
+    return results
+
+def output_as_sentences(decoder_output: List[str], corpus_sentences: List[str]):
+    token_sentences = []
+    start_index = 0
+    words_per_sentence = [len(s.split(" ")) for s in corpus_sentences]
+    print("combining word-level predictions into sentences")
+    for i in tqdm(range(len(words_per_sentence))):
+        end_index = start_index + words_per_sentence[i]
+        processed_decoder_output = [insert_wb_char(token.split("\t")[1]) for token in decoder_output[start_index:end_index]]
+        token_sentences.append(" ".join(processed_decoder_output))
+        start_index += words_per_sentence[i]
+    assert len(token_sentences) == len(corpus_sentences)
+    return token_sentences
+
+def insert_wb_char(word):
+    # Since we are training LMs on tokens, we should add word boundary characters (like GPT2)
+    wb_character = "↓" # FIXME this should go in the model config
+    if wb_character in word:
+        word = word.split(wb_character)
+        word = " ".join(word)
+    return wb_character + word
